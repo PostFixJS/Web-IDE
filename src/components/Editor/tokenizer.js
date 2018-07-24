f547dfb6c14ed378965266ed2e1eba9f63d1be22
leMaik <an@maik.tk>
2018-07-24 20:20:47 +0200

    Highlight numbers in scientific notation properly.

--- src/components/Editor/tokenizer.js
+++ src/components/Editor/tokenizer.js
@@ -49,6 +49,8 @@ export default {
 
       // numbers
       [/\d+\.\d+/, 'number.float'],
+      [/\d+e-\d+/, 'number.float'],
+      [/\d+e\+?\d+/, 'number'],
       [/\d+/, 'number'],
 
       // strings
