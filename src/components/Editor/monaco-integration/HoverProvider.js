c115b0ec7786f9485dd6b691f6308a0f110274ef
leMaik <an@maik.tk>
2018-08-12 14:52:23 +0200

    Show variable documentation when hovering a variable.

--- src/components/Editor/monaco-integration/HoverProvider.js
+++ src/components/Editor/monaco-integration/HoverProvider.js
@@ -10,13 +10,28 @@ export default {
 
     if (token != null) {
       if (token.tokenType === 'REFERENCE') {
-        let docs = builtIns.filter((doc) => doc.name === token.token)
+        let docs = builtIns.functions.filter((doc) => doc.name === token.token)
         if (docs.length === 0) { // built-in functions take precedence
           const functions = DocParser.getFunctions(code)
           docs = functions.filter((doc) => doc.name === token.token)
         }
         if (docs.length > 0) {
           const usageMessages = getFunctionHoverMessages(docs)
+          const pos = token
+
+          return {
+            range: new monaco.Range(pos.line + 1, pos.col + 1, pos.line + 1, pos.col + 1 + pos.token.length),
+            contents: usageMessages
+          }
+        }
+
+        docs = builtIns.variables.filter((doc) => doc.name === token.token)
+        if (docs.length === 0) { // built-in variables take precedence
+          const variables = DocParser.getVariables(code)
+          docs = variables.filter((doc) => doc.name === token.token)
+        }
+        if (docs.length > 0) {
+          const usageMessages = getVariableHoverMessage(docs)
           const pos = token
 
           return {
@@ -53,6 +68,22 @@ function getFunctionHoverMessages (functionDocs) {
         doc.description,
         ...doc.params.map((param) =>`*@param* \`${param.name}\`${param.description ? ` – ${param.description}` : ''}`),
         ...doc.returns.map((ret) =>`*@return* ${ret.description ? ret.description : `\`\`\`${ret.type}\`\`\``}`)
+      ].join('  \n')
+    }
+  }))
+}
+
+/**
+ * Generate markdown text for documentation of a variable.
+ * @param {object[]} variableDocs DocParser output objects for a variable
+ * @returns Array of markdown objects that document the variable
+ */
+function getVariableHoverMessage (variableDocs) {
+  return [].concat(...variableDocs.map((doc) => {
+    return {
+      value: [
+        `\`\`\`postfix\n${doc.name}\n\`\`\``,
+        doc.description
       ].join('  \n')
     }
   }))
