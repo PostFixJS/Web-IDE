a894b3702971ab3ece80eb83013916ec684080d2
Maik Marschner <m.marschner@wertarbyte.com>
2018-08-16 17:42:40 +0200

    Scroll through the repl history when pressing arrow up/down keys.

--- src/components/Repl/Repl.js
+++ src/components/Repl/Repl.js
@@ -24,10 +24,30 @@ const styles = {
   }
 }
 
+function findIndexFromEnd (arr, predicate, startIndex = arr.length - 1) {
+  for (let i = startIndex; i >= 0; i--) {
+    if (predicate(arr[i], i, arr)) {
+      return i
+    }
+  }
+  return -1
+}
+
+function findIndex (arr, predicate, startIndex = 0) {
+  for (let i = startIndex; i < arr.length; i++) {
+    if (predicate(arr[i], i, arr)) {
+      return i
+    }
+  }
+  return -1
+}
+
 export default class Repl extends React.Component {
   state = {
     lines: [],
-    running: false
+    running: false,
+    currentInput: '',
+    oldInputIndex: -1
   }
   interpreter = new Interpreter()
 
@@ -46,8 +66,61 @@ export default class Repl extends React.Component {
           ...state.lines,
           { type: 'input', value: code }
         ],
-        running: true
+        running: true,
+        oldInputIndex: -1
       }))
+    }, 'editorTextFocus')
+
+    editor.addCommand(monaco.KeyCode.UpArrow, () => {
+      if (this.state.running) return
+
+      const { oldInputIndex, lines } = this.state
+
+      if (oldInputIndex === -1) {
+        const lastInput = findIndexFromEnd(lines, ({type}) => type === 'input')
+        if (lastInput >= 0) {
+          this.setState({
+            oldInputIndex: lastInput,
+            currentInput: editor.getValue()
+          }, () => {
+            editor.setValue(lines[lastInput].value)
+            editor.setPosition({ lineNumber: 1, column: lines[lastInput].value.length + 1 })
+          })
+        }
+      } else if (oldInputIndex > 0) {
+        const lastInput = findIndexFromEnd(lines, ({type}) => type === 'input', oldInputIndex - 1)
+        if (lastInput >= 0) {
+          this.setState({ oldInputIndex: lastInput }, () => {
+            editor.setValue(lines[this.state.oldInputIndex].value)
+            editor.setPosition({ lineNumber: 1, column: lines[this.state.oldInputIndex].value.length + 1 })
+          })
+        }
+      }
+    }, 'editorTextFocus')
+
+    editor.addCommand(monaco.KeyCode.DownArrow, () => {
+      if (this.state.running) return
+
+      const { oldInputIndex, lines } = this.state
+
+      if (oldInputIndex >= 0) {
+        const nextInput =  findIndex(lines, ({type}) => type === 'input', oldInputIndex + 1)
+        if (nextInput >= 0) {
+          this.setState({
+            oldInputIndex: nextInput
+          }, () => {
+            editor.setValue(lines[nextInput].value)
+            editor.setPosition({ lineNumber: 1, column: lines[nextInput].value.length + 1 })
+          })
+        } else {
+          this.setState({
+            oldInputIndex: -1
+          }, () => {
+            editor.setValue(this.state.currentInput)
+            editor.setPosition({ lineNumber: 1, column: this.state.currentInput.length + 1 })
+          })
+        }
+      }
     }, 'editorTextFocus')
 
     editor.onDidAttemptReadOnlyEdit((e) => {
