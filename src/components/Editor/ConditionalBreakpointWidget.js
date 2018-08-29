965004dbcfcbb3851f7131e9e5a46b8f190902f3
Maik Marschner <an@maik.tk>
2018-08-29 10:44:29 +0200

    Mount a react component into the conditional breakpoint widget.

--- src/components/Editor/ConditionalBreakpointWidget.js
+++ src/components/Editor/ConditionalBreakpointWidget.js
@@ -1,4 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { ZoneWidget } from 'monaco-editor/esm/vs/editor/contrib/zoneWidget/zoneWidget'
+import OneLineEditor from '../OneLineEditor';
+
+class Widget extends React.Component {
+  editorDidMount = (editor) => {
+    setImmediate(() => {
+      editor.layout()
+      editor.focus()
+    })
+  }
+
+  render () {
+    return (
+      <div style={{ display: 'flex' }}>
+        <select>
+          <option value='expression'>Expression</option>
+          <option value='hit'>Hit count</option>
+          <option value='log'>Log a message</option>
+        </select>
+        <OneLineEditor
+          language='postfix'
+          editorDidMount={this.editorDidMount}
+        />
+      </div>
+    )
+  }
+}
 
 export default class ConditionalBreakpointWidget extends ZoneWidget {
   constructor (editor) {
@@ -9,8 +37,8 @@ export default class ConditionalBreakpointWidget extends ZoneWidget {
     })
   }
 
-  _fillContainer(container) {
-    // TODO
+  _fillContainer (container) {
+    ReactDOM.render(<Widget />, container)
   }
 
   _onWidth(width) {
