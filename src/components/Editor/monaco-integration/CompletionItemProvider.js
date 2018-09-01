e558a072f98cb29536dcbf164b5c0a45b14a8cf3
leMaik <an@maik.tk>
2018-09-01 12:58:08 +0200

    Implement item completion for functions generated by datadef.

--- src/components/Editor/monaco-integration/CompletionItemProvider.js
+++ src/components/Editor/monaco-integration/CompletionItemProvider.js
@@ -7,6 +7,7 @@ export default {
     const code = model.getValue()
     const functions = DocParser.getFunctions(code)
     const variables = DocParser.getVariables(code)
+    const datadefs = DocParser.getDatadefs(code)
 
     return [
       ...functions.map((fun) => ({
@@ -30,7 +31,8 @@ export default {
         label: fun.name,
         kind: monaco.languages.CompletionItemKind.Variable,
         documentation: fun.description
-      }))
+      })),
+      ...datadefs.reduce((items, datadef) => [...items, ...getDatadefCompletionItems(datadef)], [])
     ]
   }
 }
@@ -66,5 +68,116 @@ export function getFunctionHoverMessage (doc) {
       ...doc.params.map((param) => `*@param* \`${param.name}\`${param.description ? ` – ${param.description}` : ''}`),
       ...doc.returns.map((ret) => `*@return* ${ret.description ? ret.description : `\`\`\`${ret.type}\`\`\``}`)
     ].join('  \n')
+  }
+}
+
+function getDatadefCompletionItems (datadef) {
+  const datadefName = datadef.name.toLowerCase().substr(1)
+  if (datadef.type === 'union') {
+    return [{
+      label: `${datadefName}?`,
+      kind: monaco.languages.CompletionItemKind.Function,
+      detail: '( obj :Obj -> :Bool ) fun',
+      documentation: {
+        value: `Check if the given object is an instance of ${datadef.name}.`
+      }
+    }]
+  } else {
+    const constructorDoc = {
+      description: `Create an instance of ${datadef.name}.`,
+      params: datadef.fields,
+      returns: [{
+        type: datadef.name,
+        description: `New ${datadef.name}`
+      }]
+    }
+    const typecheckDoc = {
+      description: `Check if the given object is an instance of ${datadef.name}.`,
+      params: [{
+        name: 'obj',
+        type: ':Obj',
+        description: 'Object'
+      }],
+      returns: [{
+        type: ':Bool',
+        description: `True if the object is an instance of ${datadef.name}, false otherwise`
+      }]
+    }
+    return [{
+      label: `${datadefName}.new`,
+      kind: monaco.languages.CompletionItemKind.Constructor,
+      detail: getFunctionSignature(constructorDoc),
+      documentation: getFunctionHoverMessage(constructorDoc)
+    }, {
+      label: `${datadefName}?`,
+      kind: monaco.languages.CompletionItemKind.Function,
+      detail: getFunctionSignature(typecheckDoc),
+      documentation: getFunctionHoverMessage(typecheckDoc)
+    }, ...datadef.fields.map((field) => {
+      const getterDoc = {
+        description: `Get the ${field.name} field of the given ${datadef.name} instance.`,
+        params: [{
+          name: datadefName,
+          type: datadef.name,
+          description: `${datadef.name} instance`
+        }],
+        returns: [{
+          type: field.type,
+          description: field.description
+        }]
+      }
+      return {
+        label: `${datadefName}.${field.name}`,
+        kind: monaco.languages.CompletionItemKind.Function,
+        detail: getFunctionSignature(getterDoc),
+        documentation: getFunctionHoverMessage(getterDoc)
+      }
+    }), ...datadef.fields.map((field) => {
+      const setterDoc = {
+        description: `Set the ${field.name} field of the given ${datadef.name} instance.`,
+        params: [{
+          name: datadefName,
+          type: datadef.name,
+          description: `${datadef.name} instance`
+        }, {
+          name: field.name,
+          type: field.type,
+          description: field.description
+        }],
+        returns: [{
+          type: datadef.name,
+          description: `Updated ${datadef.name} instance`
+        }]
+      }
+      return {
+        label: `${datadefName}-${field.name}-set`,
+        kind: monaco.languages.CompletionItemKind.Function,
+        detail: getFunctionSignature(setterDoc),
+        documentation: getFunctionHoverMessage(setterDoc)
+      }
+    }), ...datadef.fields.map((field) => {
+      const updaterDoc = {
+        description: `Update the ${field.name} field of the given ${datadef.name} instance.`,
+        params: [{
+          name: datadefName,
+          type: datadef.name,
+          description: `${datadef.name} instance`
+        }, {
+          name: 'updater',
+          type: ':ExeArr',
+          description: `Update function that will be called with the current ${field.name} value (${field.type}) and must return a new value`
+        }],
+        returns: [{
+          type: datadef.name,
+          description: `Updated ${datadef.name} instance`
+        }]
+      }
+      return {
+        label: `${datadefName}-${field.name}-do`,
+        kind: monaco.languages.CompletionItemKind.Function,
+        detail: getFunctionSignature(updaterDoc),
+        documentation: getFunctionHoverMessage(updaterDoc)
+      }
+    })]
   }
 }
