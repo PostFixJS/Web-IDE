5f9f3dcdf5df22db11908d58168b99377eb25421
leMaik <an@maik.tk>
2018-09-23 17:05:33 +0200

    Queue to show function callbacks, allow async operators in them.

--- src/interpreter/canvas.js
+++ src/interpreter/canvas.js
@@ -19,38 +19,83 @@ export function registerBuiltIns (interpreter) {
     const left = (window.outerWidth - width.value) / 2 + window.screenX
     
     const { cancel, token: cancelToken } = createCancellationToken()
+    let rejectAll
     const win = window.open('about:blank', '_blank', `top=${top},left=${left},width=${width.value},height=${height.value}`)
     win.document.title = title.value
     win.addEventListener('unload', cancel)
+
+    // queue interpreter invocations to prevent race conditions due to async execution
+    const enqueue = (() => {
+      let queue = Promise.resolve()
+      return (fn) => {
+        queue = queue.then(fn)
+        return queue
+      }
+    })()
+    let cancelQueue = null
+    cancelToken.onCancel(() => {
+      if (cancelQueue != null) {
+        cancelQueue()
+      }
+    })
+    const runInQueue = async (obj) => {
+      const { promise, cancel } = interpreter.runObj(obj)
+      cancelQueue = cancel
+      await promise
+      cancelQueue = null
+    }
     
     let state = initialState
-    
-    // TODO queue interpreter calls to prevent race conditions due to async execution
+
     const onDraw = keyGet(callbacks, new types.Sym('on-draw'), null)
     if (onDraw) {
-      // global requestAnimationFrame
-        const redraw = () => requestAnimationFrame(() => {
-          if (cancelToken.cancelled) return
+      const redraw = () => {
+        if (cancelToken.cancelled) return
+        enqueue(async () => {
+          // TODO prevent infinite loops, allow debugging (i.e. use a runner)
           interpreter._stack.push(state)
-          Array.from(onDraw.execute(interpreter)) // TODO prevent infinite loops
-          state = interpreter._stack.pop()
-          win.state = state
-          if (!cancelToken.cancelled) redraw()
-        })
+          try {
+            await runInQueue(onDraw)
+            if (cancelToken.cancelled) return
+            state = interpreter._stack.pop()
+            // global requestAnimationFrame
+              requestAnimationFrame(() => {
+                win.state = state
+                if (!cancelToken.cancelled) redraw()
+              })
+            } catch (e) {
+              cancel()
+              if (e.message !== 'cancelled') {
+                rejectAll(e)
+              }
+            }
+          })
+        }
         setImmediate(redraw)
       }
 
-      // const onKeyPress = keyGet(callbacks, new types.Sym('on-key-press'), null)
-      // if (onKeyPress) {
-      //   win.onkeypress = (e) => {
-      //     interpreter._stack.push(new types.Str(e.key))
-      //     Array.from(onKeyPress.execute(interpreter)) // TODO prevent infinite loops
-      //   }
-      // }
+      const onKeyPress = keyGet(callbacks, new types.Sym('on-key-press'), null)
+      if (onKeyPress) {
+        win.onkeypress = (e) => {
+          enqueue(async () => {
+            // TODO prevent infinite loops, allow debugging (i.e. use a runner)
+            interpreter._stack.push(new types.Str(e.key))
+            try {
+              await runInQueue(onKeyPress)
+            } catch (e) {
+              cancel()
+              if (e.message !== 'cancelled') {
+                rejectAll(e)
+              }
+            }
+          })
+        }
+      }
 
       yield {
         cancel,
-        promise: new Promise((resolve) => {
+        promise: new Promise((resolve, reject) => {
+          rejectAll = reject
           win.addEventListener('unload', () => resolve())
           cancelToken.onCancel(() => win.close())
         })
