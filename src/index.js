614cb1f0a2f999a97e9453e03a7de8516da7876f
leMaik <an@maik.tk>
2018-09-27 23:49:55 +0200

    Disable the service worker for now.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 
 ReactDOM.render((
@@ -11,4 +11,4 @@ ReactDOM.render((
     <App />
   </Provider>), document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
