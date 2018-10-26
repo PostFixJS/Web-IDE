a6ebd501a2010cc327e35797273077f91d044fa1
leMaik <an@maik.tk>
2018-10-26 22:48:20 +0200

    Fix typo in test messages.

--- src/components/Editor/tests.js
+++ src/components/Editor/tests.js
@@ -8,7 +8,7 @@ export function getTestResultMessage ({ passed, type, params }) {
     return `✗ Expected \`${expected.toString()}\` but got \`${actual.toString()}\``
   } else if (type === 'test!=') {
     const {actual, expected } = params
-    return `✗ Expected value not to equal \`${expected.toString()}\` but got \`${expected.toString()}\``
+    return `✗ Expected value not to equal \`${expected.toString()}\` but got \`${actual.toString()}\``
   } else if (type === 'test~=') {
     const { actual, expected, tolerance } = params
     return `✗ Expected value to to equal \`${expected.toString()}\` (within a tolerance of ±${tolerance.toString()}) but got \`${actual.toString()}\``
