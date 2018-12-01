d1b2bf59928e8996ef950baa1446dfeaaf96c840
leMaik <an@maik.tk>
2018-12-01 23:28:27 +0100

    Only suggest generating a function when the current line is empty.

--- src/components/Editor/monaco-integration/snippets/FunctionSnippet.js
+++ src/components/Editor/monaco-integration/snippets/FunctionSnippet.js
@@ -2,19 +2,26 @@ import * as monaco from 'monaco-editor'
 
 export default {
   provideCompletionItems: (model, position) => {
-    return [{
-      label: 'Generate a function',
-      kind: monaco.languages.CompletionItemKind.Snippet,
-      documentation: 'Generate a function.',
-      insertText: {
-        value: '${1:name}: (${2:parameters}) {\n    $0\n} fun' // eslint-disable-line no-template-curly-in-string
-      },
-      range: {
-        startLineNumber: position.lineNumber,
-        startColumn: position.column,
-        endLineNumber: position.lineNumber,
-        endColumn: position.column + 3
-      }
-    }]
+    const items = []
+
+    const lineIsEmputy = model.getLineContent(position.lineNumber).trim().length === 0
+    if (lineIsEmputy) {
+      items.push({
+        label: 'Generate a function',
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        documentation: 'Generate a function.',
+        insertText: {
+          value: '${1:name}: (${2:parameters}) {\n    $0\n} fun' // eslint-disable-line no-template-curly-in-string
+        },
+        range: {
+          startLineNumber: position.lineNumber,
+          startColumn: position.column,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column + 3
+        }
+      })
+    }
+
+    return items
   }
 }
