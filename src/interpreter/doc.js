cd1b58e0a13b8de5524b2e6853d38e942c13cfe2
leMaik <an@maik.tk>
2018-12-17 23:10:38 +0100

    Remove built-in variables from default built-ins.

--- src/interpreter/doc.js
+++ src/interpreter/doc.js
@@ -1,7 +1,7 @@
 import builtIns from 'postfixjs/doc/operators'
 
 export const functions = [ ...builtIns.functions ]
-export const variables = [ ...builtIns.variables ]
+export const variables = []
 export const symbols = []
 
 export function registerFunctions (...functionSignatures) {
