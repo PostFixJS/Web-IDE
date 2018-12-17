bab040b812ccd42819ec63b275d7ccdb0c01c86c
leMaik <an@maik.tk>
2018-12-17 01:16:29 +0100

    Add a search function for the documentation.

--- src/components/Documentation/Documentation.js
+++ src/components/Documentation/Documentation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import * as JsSearch from 'js-search'
 import * as builtIns from '../../interpreter/doc'
 import FunctionEntry from './FunctionEntry'
 
@@ -9,19 +10,56 @@ const styles = (theme) => ({
     background: theme.card.background,
     color: theme.card.color,
     marginLeft: 5,
-    padding: '8px 16px 16px'
+    padding: '8px 16px 16px',
+    minHeight: '100%'
+  },
+  noResults: {
+    textAlign: 'center',
+    marginTop: 24,
+    opacity: 0.5,
+    fontSize: '14px'
   }
 })
 
+let jsSearchIndex
+
 class Documentation extends React.PureComponent {
+  state = { search: '' }
+
+  componentDidMount () {
+    jsSearchIndex = new JsSearch.Search('id')
+    jsSearchIndex.addIndex('name')
+    jsSearchIndex.addIndex('description')
+    jsSearchIndex.addIndex(['params', 'description'])
+    jsSearchIndex.addIndex(['returns', 'description'])
+    jsSearchIndex.addDocuments(builtIns.functions.map((fun) => ({
+      ...fun,
+      id: getFunctionId(fun)
+    })))
+  }
+
+  handleSearchInput = (e) => this.setState({ search: e.target.value })
+
   render () {
     const { classes } = this.props
-    const functions = [...builtIns.functions].sort((a, b) => a.name.localeCompare(b.name))
+    const { search } = this.state
+
+    let functions
+    if (search.trim() === '') {
+      functions = [...builtIns.functions]
+        .sort((a, b) => a.name.localeCompare(b.name))
+    } else {
+      functions = jsSearchIndex.search(search)
+    }
 
     return (
       <div className={classes.root}>
-        {functions.map((fun) => {
-          const id = getFunctionId(fun)
+        <input
+          value={search}
+          onChange={this.handleSearchInput}
+        />
+        {functions.length > 0 ? functions.map((fun) => {
+          const id = fun.id || getFunctionId(fun)
           return (
             <FunctionEntry
               key={id}
@@ -29,7 +67,11 @@ class Documentation extends React.PureComponent {
               fun={fun}
             />
           )
-        })}
+        }) : (
+          <div className={classes.noResults}>
+            There are no functions that match the search query.
+          </div>
+        )}
       </div>
     )
   }
